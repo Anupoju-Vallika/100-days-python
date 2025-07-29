a4b4421e72d502eb0566923db4d918f350384b4b
Anupoju-Vallika <vallikaanupoju@gmail.com>
2025-07-29 14:54:03 +0530

    Create 01_NameGenerator.py

---
+++ 01_day_python.py/01_NameGenerator.py
@@ -0,0 +1,4 @@
+print("🎶 Welcome to the Vibrant Vibes Name Creator!.")
+animal = input("Which animal do you like most?  :")
+color = input("What's your favorite color combined with an animal? (e.g., BlueTiger, PinkElephant)  :")
+print("Your band name could be: " + color+animal) 
