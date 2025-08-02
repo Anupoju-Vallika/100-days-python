7ad8d0d1a2fec53b1526cb65a605cb9b04259acf
Anupoju-Vallika <vallikaanupoju@gmail.com>
2025-08-02 20:55:36 +0530

    Create tollywood_data.py

---
+++ 03_day_python/10_Higher _or_Lower game/tollywood_data.py
@@ -0,0 +1,14 @@
+# tollywood_data.py
+
+data = [
+    {"name": "Prabhas", "followers": 21},
+    {"name": "Allu Arjun", "followers": 25},
+    {"name": "Mahesh Babu", "followers": 12},
+    {"name": "Ram Charan", "followers": 10},
+    {"name": "N. T. Rama Rao Jr.", "followers": 8},
+    {"name": "Pooja Hegde", "followers": 23},
+    {"name": "Rashmika Mandanna", "followers": 28},
+    {"name": "Samantha Ruth Prabhu", "followers": 27},
+    {"name": "Vijay Deverakonda", "followers": 20},
+    {"name": "Nani", "followers": 5}
+]
