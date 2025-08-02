e2e8c50c5148b3600e5a6a02e1a22090bb73f34b
Anupoju-Vallika <vallikaanupoju@gmail.com>
2025-08-02 20:55:20 +0530

    Create data.py

---
+++ 03_day_python/12_quiz_project/data.py
@@ -0,0 +1,31 @@
+question_data = [
+    {
+        "category": "India: General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Constitution of India came into effect on January 26, 1950.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "India: General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "New Delhi is the capital city of India.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "India: General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Ganges is the longest river in India.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    } ]
